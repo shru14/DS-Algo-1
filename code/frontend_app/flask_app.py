8c7c53676add4fc802bbe6afcf4b9f12f9552a16
bprtdaniel <daniel@boppert.net>
2024-02-27 10:57:09 +0100

    adding a first flask forms app for input data to be sourced into python list
    In the spirit of Open Source: I stole most of this from a website, will link later

---
+++ code/frontend_app/flask_app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+import secrets
+
+
+app = Flask(__name__)
+
+
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+    
