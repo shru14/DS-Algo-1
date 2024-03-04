f70ba0e07b8eb7d7f74712dc94b18cc2f8d717a7
bprtdaniel <daniel@boppert.net>
2024-03-04 22:32:29 +0100

    some fixes

--- code/algo_stuff/gs.py
+++ code/algo_stuff/gs.py
@@ -19,6 +19,7 @@ prof_preferences = functions.preferences(data[::-1])
 # print(prof_preferences)
 
 
+# Either run with defined sets student_pref and prof_pref as below, or pull from "data" (create_data output).
 
 
 student_pref = {
@@ -36,31 +37,40 @@ prof_pref = {
 
 # Now, try to implement a basic gale/shapley allocation based on the dictionries of preferences. This uses the list of lists as input and matches according to the above create set of preferences for both parties.
 
-# Disclaimer: first parts I succeeded to do by myself and with the pseudocode from the lecture, afterwards, I had to be aided extensively by our lord and savior, ChatGPT 
+
+
+# Disclaimer: first parts I succeeded to do by myself and with the pseudocode from the lecture.
+# Afterwards, I had to be aided by our lord and savior, ChatGPT.
+
+# Added a million comments, as I tried to explain it to myself. 
+# Most important part: Variables of student and current_match, which are compared to each other. If student is better offer, replace current match. Vice versa.
+
+
 def gs_match(students_preferences, prof_preferences):
-    matches = {}
+    matches = {} # Initatiate dictionary that in the returns all stable matches. Initiated as empty, since we don't have any yet.
     
-    free_student = list(students_preferences.keys()) # Initiate the student as free. Returns the keys from matching dictionary and transfomrs to list.
+    free_student = list(students_preferences.keys()) # Initiate the student as free. Returns the keys from matching dictionary and transforms to list. 
     
-    while free_student:
-        student = free_student.pop(0) # .pop takes and removes the students at index 0 from the free students list. 
-        prof = students_preferences[student].pop(0) # Take the first preference of the first student.
+    while free_student: 
+        student = free_student.pop(0) # .pop takes and removes the student at index 0 from the free students list. Assign to student, this equals S in the lecture.
+        prof = students_preferences[student].pop(0) # Take the first preference of the first student. This is a professor, as it draws from the student preferences dictionary.
         
         current_match = matches.get(prof) # Assign the 'current match' to a variable to check if professor is currently matched. Current match is a professor.
+        # In first iteration, this is always NOT, as the matches dictionary was initiated as empty.
         
-        if not current_match:
-            matches[prof] = student # If not, match them to each other. Create tentative match, update matches dictionary, as a match is better than the current no match at all.
-        else:
+        if not current_match: # In case match does not exist yet.
+            matches[prof] = student # Match them to each other. Create tentative match, update matches dictionary, as any match is better than the current no match at all.
+        else: # If there is a match already:
             prof_pref_list = prof_preferences[prof] # If professor is matched otherwise already, get the preferences of the prof. These contain names of students in order of preferences. Assign to variable.
-            if prof_pref_list.index(current_match) > prof_pref_list.index(student): # Check if the current match is higher than the current student, which was defined before and is free.
-                matches[prof] = student
-                free_student.append(current_match) # As current students is preferred over current match, current student is free again and appended to free list.
+            if prof_pref_list.index(current_match) > prof_pref_list.index(student): # Check if the current match is higher than the current student, which was defined before and is free. Higher, as in index further to the right, so less preferable. 
+                matches[prof] = student # Final match, outcome of comparison, match and add prof with student.
+                free_student.append(current_match) # As new student is preferred over current match, current match is free again and appended to free list.
             else:
-                free_student.append(student)
+                free_student.append(student) # Vice versa of above.
     return matches
 
 
+# Run it on above example, return and print matches.
 pairs = gs_match(students_preferences = student_pref, prof_preferences = prof_pref)
 print(pairs)
 
-# Either run with defined set student_pref as above or pull from "data" (create_data output)
