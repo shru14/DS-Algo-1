644893fc078e74528b8f4e7258976196eb3f67fd
Daniyar.Imanaliev <imanalievdu@gmail.com>
2024-03-05 13:09:18 +0100

    Dan's Algo

--- code/algo_stuff/gs.py
+++ code/algo_stuff/gs.py
@@ -74,3 +74,34 @@ def gs_match(students_preferences, prof_preferences):
 pairs = gs_match(students_preferences = student_pref, prof_preferences = prof_pref)
 print(pairs)
 
+#Daniyar's Code
+def stable_matching():
+    while(len(free_stud) > 0):
+        for student in free_stud:
+            start_matching(student)
+
+def start_matching(student):
+    for professor in student_preferences[student]:
+
+        taken_match = [pair for pair in tentative_pairs if professor in pair]
+
+        if (len(taken_match) == 0):
+            tentative_pairs.append([student, professor])
+            free_stud.remove(student)
+            break
+        elif (len(taken_match) > 0):
+            current_student = prof_preferences[professor].index(taken_match[0][0])
+
+            potential_student = prof_preferences[professor].index(student)
+
+            if (current_student > potential_student):
+                free_stud.remove(student)
+
+                free_stud.append(taken_match[0][0])
+
+                taken_match[0][0] = student
+                break
+
+def main():
+    init_free_stud():
+    stable_matching()
