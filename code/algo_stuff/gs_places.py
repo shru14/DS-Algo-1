ec7b12010d87a1ba975a261918c9f2a1aa53872b
Daniyar.Imanaliev <imanalievdu@gmail.com>
2024-03-10 12:04:58 +0100

    Added comments
    Added comments to gs_places

--- code/algo_stuff/gs_places.py
+++ code/algo_stuff/gs_places.py
@@ -1,6 +1,7 @@
 def gs_match(students_preferences, prof_preferences, places):
     matches = {}
     remaining_places = places.copy() #Copying places here
+    unmatched_students = [] #Added empty list for unmatched students
 
     free_student = list(
         students_preferences.keys())  # Initiate the student as free. Returns the keys from matching dictionary and transforms to list.
@@ -8,21 +9,22 @@ def gs_match(students_preferences, prof_preferences, places):
     while free_student:
         student = free_student.pop(
             0)  # .pop takes and removes the student at index 0 from the free students list. Assign to student, this equals S in the lecture.
-        if students_preferences[student]:
+        if students_preferences[student]: # Modified to loop to check whether a student unmatched
             prof = students_preferences[student].pop(
                 0)  # Take the first preference of the first student. This is a professor, as it draws from the student preferences dictionary.
+            #Added if-loop to check if professor has free places
             if prof not in matches:
                 matches[prof] = [student]
-                remaining_places[prof] -= 1
-            elif remaining_places[prof] > 0:
+                remaining_places[prof] -= 1 #Here we decrease places after match
+            elif remaining_places[prof] > 0: #Here we match
                 matches[prof].append(student)
                 remaining_places[prof] -= 1
-            else:
+            else: #Here we matching students and profs
                 prof_pref_list = prof_preferences[
                     prof]  # If professor is matched otherwise already, get the preferences of the prof. These contain names of students in order of preferences. Assign to variable.
                 current_match = matches[prof][0]
 
-                if prof_pref_list.index(current_match) > prof_pref_list.index(student) and remaining_places[prof] >0:
+                if prof_pref_list.index(current_match) > prof_pref_list.index(student): #and remaining_places[prof] >0:
                     # Check if the current match is higher than the current student, which was defined before and is free. Higher, as in index further to the right, so less preferable.
                     matches[prof] = student  # Final match, outcome of comparison, match and add prof with student.
                     free_student.append(
@@ -31,7 +33,9 @@ def gs_match(students_preferences, prof_preferences, places):
                 else:
                     free_student.append(student)  # Vice versa of above.
         else:
-            continue
+            unmatched_students.append(student)
+    print("Unmatched Students:", unmatched_students)
+
     return matches
 
 student_pref = {
